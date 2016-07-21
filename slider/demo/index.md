eacc46c9e6ddcc16a598b6f56e2f1c05fbf5b396
Yusen <rainforest92@126.com>
2016-07-21 23:08:54 +0800

    [added] grid & step

--- slider/demo/index.md
+++ slider/demo/index.md
@@ -4,7 +4,10 @@
 <div class="m-example"></div>
 
 ```xml
-<slider />
+<div class="g-row">
+    <div class="g-col g-col-6"><slider /></div>
+    <div class="g-col g-col-6"><slider value=20 /></div>
+</div>
 ```
 
 ### 只读和禁用
@@ -13,7 +16,35 @@
 
 ```xml
 <div class="g-row">
-    <div class="g-col g-col-6"><slider percent=20 readonly /></div>
-    <div class="g-col g-col-6"><slider percent=20 disabled /></div>
+    <div class="g-col g-col-6"><slider value=20 readonly /></div>
+    <div class="g-col g-col-6"><slider value=20 disabled /></div>
 </div>
+```
+
+### 最大值和最小值
+
+<div class="m-example"></div>
+
+```xml
+<slider value={value} min=10 max=50 />
+<p>{value}</p>
+```
+
+### 连续和间隔
+
+<div class="m-example"></div>
+
+```xml
+<div class="g-row">
+    <div class="g-col g-col-6"><slider /></div>
+    <div class="g-col g-col-6"><slider step=20 /></div>
+</div>
+```
+
+### 事件
+
+<div class="m-example"></div>
+
+```xml
+<slider on-change={console.log('on-change', '$event.value', $event.value)} />
 ```
--- slider/index.js
+++ slider/index.js
@@ -7,7 +7,11 @@ import 'rgui-ui-drag';
  * @class Slider
  * @extend Component
  * @param {object}                  options.data                     =  绑定属性
- * @param {string='Hello World'}    options.data.message            <=> 消息
+ * @param {number=0}                options.data.value              <=> 数值
+ * @param {number=0}                options.data.min                <=> 最小值
+ * @param {number=100}              options.data.max                <=> 最大值
+ * @param {number=0}                options.data.step               <=> 间隔
+ * @param {boolean=false}           options.data.readonly            => 是否只读
  * @param {boolean=false}           options.data.disabled            => 是否禁用
  * @param {boolean=true}            options.data.visible             => 是否显示
  * @param {string=''}               options.data.class               => 补充class
@@ -21,35 +25,59 @@ const Slider = Component.extend({
      */
     config() {
         this.data = Object.assign({
-            percent: 0,
+            value: 0,
+            min: 0,
+            max: 100,
+            step: 0,
+            _grid: { x: 0, y: 0 },
         }, this.data);
         this.supr();
 
-        this.$watch('percent', (newValue, oldValue) => {
+        this.$watch('value', (newValue, oldValue) => {
             this.$emit('change', {
                 sender: this,
-                percent: newValue,
+                value: newValue,
             });
         });
+    },
+    computed: {
+        percent: {
+            get() {
+                return (this.data.value - this.data.min)/(this.data.max - this.data.min)*100;
+            },
+            set(percent) {
+                let value = +this.data.min + (this.data.max - this.data.min)*percent/100;
+                if (this.data.step)
+                    value = Math.round(value/this.data.step)*this.data.step;
+                this.data.value = value;
+            },
+        },
     },
     /**
      * @private
      */
     _onMouseDown($event) {
-        if(this.data.readonly || this.data.disabled)
+        if (this.data.readonly || this.data.disabled)
             return;
 
         const e = $event.event;
         const $handle = this.$refs.handle;
         const $parent = $handle.offsetParent;
-        const position = dom.getPosition($parent);
-        this.data.percent = (e.clientX - position.left)/$parent.offsetWidth*100;
+        const dimension = dom.getDimension($parent, 'center');
+
+        this.$set('percent', (e.clientX - dimension.left)/dimension.width*100);
+    },
+    /**
+     * @private
+     */
+    _onDragStart($event) {
+        this.data._grid.x = this.data.step/(this.data.max - this.data.min)*$event.range.width;
     },
     /**
      * @private
      */
     _onDrag($event) {
-        this.data.percent = $event.left/$event.range.right*100;
+        this.$set('percent', $event.left/$event.range.width*100);
     },
 });
 
